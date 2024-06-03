a53e792741724a775b055242e29aa8dde7206b26
umehararyouta <c1p31018@bunkyo.ac.jp>
2024-06-04 08:24:22 +0900

    とりあえずうごいた

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useEffect} from 'react';
+import 'blockly/blocks';
+import * as Blockly from 'blockly/core';
+import * as libraryBlocks from 'blockly/blocks';
+import {javascriptGenerator} from 'blockly/javascript';
+import * as En from 'blockly/msg/en';
+Blockly.setLocale(En);
 function App() {
+  useEffect(() => {  
+    const toolbox = {
+    "kind": "flyoutToolbox",
+    "contents": [
+      {
+        "kind": "block",
+        "type": "controls_if"
+      },
+      {
+        "kind": "block",
+        "type": "controls_repeat_ext"
+      },
+      {
+        "kind": "block",
+        "type": "logic_compare"
+      },
+      {
+        "kind": "block",
+        "type": "math_number"
+      },
+      {
+        "kind": "block",
+        "type": "math_arithmetic"
+      },
+      {
+        "kind": "block",
+        "type": "text"
+      },
+      {
+        "kind": "block",
+        "type": "text_print"
+      },
+    ]
+  };
+  const workspace = Blockly.inject('blocklyDiv', {toolbox: toolbox});
+},[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="blocklyDiv"style={{ height: '480px', width: '600px' }}></div>
+    
   );
 }
 
